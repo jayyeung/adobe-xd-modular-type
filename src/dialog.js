56afa54495cce66799ab0687cbb1a738f4f3506a
Jason Yeung <jsy9@sfu.ca>
2018-12-01 12:37:17 -0800

    Separate dialog into own class

---
+++ src/dialog.js
@@ -0,0 +1,32 @@
+import { observable, action } from 'mobx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Dialog {
+    @observable active = false;
+    target = null;
+
+    createDialog = (component) => {
+        if (this.target == null) {
+            this.target = document.createElement('dialog');
+            ReactDOM.render(component, this.target);
+        }
+        return this.target;
+    }
+
+    async getDialog() {
+        this._setActive(true);
+        const res = await document.body
+            .appendChild(this.target)
+            .showModal();
+        this._setActive(false);
+        return res;
+    }
+
+    @action _setActive = (active) => { this.active = active; }
+    close = () => { this.target.close('USER_CLOSE'); }
+    accept = () => { this.target.close('USER_ACCEPT'); }
+}
+
+const dialog = new Dialog();
+export default dialog;
--- src/main.js
+++ src/main.js
@@ -1,37 +1,24 @@
 const reactShim = require('./react-shim');
-const { observable } = require('mobx');
-const { Provider } = require('mobx-react');
 const React = require('react');
-const ReactDOM = require('react-dom');
 
 const styles = require('./styles/main.scss');
 const stores = require('./stores/store').default;
-const ModularType = require('./components/ModularType').default; 
+const dialog = require('./dialog').default;
+const ModularType = require('./components/ModularType').default;
 
-let dialog; let dialogOpen = observable.box(false);
-function getDialog(selection) {
-    if (dialog == null) {
-        dialog = document.createElement('dialog');
-        ReactDOM.render(
-            <Provider {...stores}>
-                <ModularType
-                    selection={selection}
-                    open={dialogOpen}
-                    dialog={dialog} />
-            </Provider>
-        , dialog);
-    }
-    return dialog;
-} 
+dialog.createDialog(
+    <ModularType
+        dialog={dialog}       
+        typeStore={stores.typeStore} 
+    />
+);
 
 module.exports.commands = {
     menuCommand: async function(selection) {
-        dialogOpen.set(true);
-        const res = await document.body.appendChild(getDialog(selection)).showModal();
-        dialogOpen.set(false);
-
+        dialog.selection = selection;
+        const res = await dialog.getDialog();
         switch(res) {
-            case 'reasonCanceled': case 'USER_CANCEL':
+            case 'reasonCanceled':
                 throw new Error('Rejected: User canceled dialog');
         }
     }
