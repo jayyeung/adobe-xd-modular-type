606d00bdc973be189bbf412dd8eeff16be0fa1c5
Jason Yeung <jsy9@sfu.ca>
2018-12-07 22:54:48 -0800

    move NumberInput component to single file

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,35 +1,41 @@
+import { computed } from 'mobx';
 import React, { Component } from 'react';
 
 class NumberInput extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' }
+        this.state = { value: '', valid: true }
     }
 
     componentDidMount() {
+        // set initial value of input
         const initValue = this.props.input.value || '';
-        this.setState({value: initValue});
+        this.setValue(initValue);
     }
 
     isNumber = (value) => ((/^(?:[1-9]\d*|0)?(?:\.\d+)?$/g).test(value));
+
+    setValue = (value) => { this.setState({value}); }
+
     _handleChange = (e) => {
         const value = e.target.value;
-        this.setState({value});
-
+        this.setValue(value);
+        
         // Temporary way to validate values :P
         // until Adobe XD implements proper number inputs
-        if (this.isNumber(value))
-            return this.props.onChange(value);
+        const isNumber = this.isNumber(value);
+        this.setState({valid: isNumber});
+        if (isNumber) return this.props.onChange(value);
     }
 
     render() {
-        const { value } = this.state
+        const { valid, value } = this.state;
         const { title, input } = this.props;
 
         return (
             <label>
-                <span>{title || ''}</span>
-                <input type='text' {...input} value={value}
+                <span className={(!valid) && 'color-red'}>{title || ''}</span>
+                <input type='text' {...input} value={this.val || value}
                     onChange={this._handleChange}/>
             </label>
         )
--- src/components/NumberInput/index.js
+++
@@ -1,2 +0,0 @@
-import Container from './NumberInput.jsx';
-export default Container;
